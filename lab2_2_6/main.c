a2dd0fe144de8c6b88a00cdcf3a882c495f88c5f
alekskorolev3 <alekskorolev3@gmail.com>
2021-03-04 01:54:06 +0300

    Initial commit

--- lab2_2_6/main.c
+++ lab2_2_6/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <math.h>
+#include "main.h"
 
-
-double Formula (int n, double x, double* sum, double* present)
-{
-    *present = *present * (-pow(x, 2))/(2*(2*n - 1)*(n - 1));
-    *sum = *sum + *present;
-    return *sum;
-}
 int main() {
-    double x = 2, e = 0.0000001;
-    //a
-
+    double x = 0, e = 0;
     int n = 2;
-    /*scanf ("%f",&x);*/
+    printf ("Enter 'x' value and measurement uncertainty 'e' \n");
+    scanf ("%lf",&x);
+    scanf ("%lf", &e);
     double sum = x;
     double present = x;
-
-    while (fabs(Formula(n, x, &sum, &present) - sin(x)) >= e)
+    printf (" Sin(x)   Decomp      N \n");
+    while (fabs(Expr(n, x, &sum, &present) - sin(x)) >= e)
     {
         printf ("%f %f %d %s", sin(x), sum, n, "\n");
         n++;
     }
+    printf ("%f %f %d %s", sin(x), sum, n, "\n");
+    printf ("%s %d %s %lf", "So, at value N, equal", n, "the expression under study differs from sin(x) less than measurement uncertainty", e);
+}
 
+double Expr (int n, double x, double* sum, double* present)
+{
+    *present = *present * (-pow(x, 2))/(2*(2*n - 1)*(n - 1));
+    *sum = *sum + *present;
+    return *sum;
 }
---
+++ lab2_2_6/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Artyom on 04.03.2021.
+//
+
+#ifndef LAB2_2_6_MAIN_H
+#define LAB2_2_6_MAIN_H
+double Expr (int n, double x, double* sum, double* present);
+#endif //LAB2_2_6_MAIN_H
